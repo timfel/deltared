15acb1fcb729d66d8da176c95317a9e0aadc05d8
MenTaLguY <mental@rydia.net>
2008-01-02 04:26:53 -0500

    start on constraint tests...

--- test/test_all.rb
+++ test/test_all.rb
@@ -1,1 +1,2 @@
+require 'test/test_constraint.rb'
 require 'test/test_variable.rb'
---
+++ test/test_constraint.rb
@@ -0,0 +1,19 @@
+require 'test/unit'
+require 'deltared'
+
+class ConstraintTest < Test::Unit::TestCase
+  def test_simple_one_way
+    a, b = DeltaRed.variables(0, 0)
+    DeltaRed.constraint! do |c|
+      c.formula(a => b) { |v| v * 2 }
+    end
+    assert_equal 0, a.value
+    assert_equal 0, b.value
+    b.value = 2
+    assert_equal 2, b.value
+    assert_equal 4, a.value
+    a.value = 3
+    assert_equal 3, a.value
+    assert_equal 2, b.value
+  end
+end
