e687264cb1995c9510aad90e7ba458f22d0528c9
MenTaLguY <mental@rydia.net>
2008-01-07 22:17:45 -0500

    move intermediate handles with endpoints

--- toys/bezier.rb
+++ toys/bezier.rb
@@ -33,10 +33,43 @@ require 'toy'
 require 'deltared'
 
 Toy.run("Bezier") do
+  endpoints = DeltaRed.variables([10, 50], [150, 50])
+  velocities = DeltaRed.variables([40, 0], [40, 0])
+
+  control_points = DeltaRed.variables(nil, nil, nil, nil)
+
+  control_points[0].constraint!(endpoints.first)
+  control_points[1].constraint!(endpoints.first, velocities.first) do |ep, vel|
+    [ ep[0] + vel[0], ep[1] + vel[1] ]
+  end
+  control_points[2].constraint!(endpoints.last, velocities.last) do |ep, vel|
+    [ ep[0] - vel[0], ep[1] - vel[1] ]
+  end
+  control_points[3].constraint!(endpoints.last)
+
   bezier = draw {}
-  handles = [[10, 50], [20, 40], [30, 60], [40, 50]].map { |x, y| knot(x, y) }
-  inputs = DeltaRed.variables(*handles.map { |h| [h.x, h.y] })
-  DeltaRed.output(*inputs) do |(x0, y0), (x1, y1), (x2, y2), (x3, y3)|
+  handles = (0..3).map { knot(0, 0) }
+
+  handles[0].when_dragged do |x, y|
+    endpoints.first.value = [x, y]
+  end
+  handles[1].when_dragged do |x, y|
+    ep = endpoints.first.value
+    velocities.first.value = [ x - ep[0], y - ep[1] ]
+  end
+  handles[2].when_dragged do |x, y|
+    ep = endpoints.last.value
+    velocities.last.value = [ ep[0] - x, ep[1] - y ]
+  end
+  handles[3].when_dragged do |x, y|
+    endpoints.last.value = [x, y]
+  end
+
+  handles.zip(control_points) do |handle, control_point|
+    DeltaRed.output(control_point) { |x, y| handle.move(x, y) }
+  end
+
+  DeltaRed.output(*control_points) do |(x0, y0), (x1, y1), (x2, y2), (x3, y3)|
     bezier.redraw do |ctx|
       ctx.move_to x0, y0
       ctx.curve_to x1, y1, x2, y2, x3, y3
@@ -46,14 +79,6 @@ Toy.run("Bezier") do
       ctx.move_to x2, y2
       ctx.line_to x3, y3
       ctx.stroke_rgb 1, 0.0, 0.0, 0.0
-    end
-  end
-  handles.zip(inputs) do |handle, input|
-    handle.when_dragged do |x, y|
-      input.value = [x, y]
-    end
-    DeltaRed.output(input) do |x, y|
-      handle.move(x, y)
     end
   end
 end
