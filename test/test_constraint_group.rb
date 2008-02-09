6c789307e5e590f25a87dad884ded7191fdca815
MenTaLguY <mental@rydia.net>
2008-02-09 17:51:17 -0500

    test nested group capture as well

--- test/test_constraint_group.rb
+++ test/test_constraint_group.rb
@@ -16,13 +16,20 @@ class ConstraintGroupTest < Test::Unit::TestCase
   def test_group_capture
     vars = DeltaRed.variables(*(0..4))
     constraints = nil
-    group = DeltaRed.group do
-      constraints = vars.map { |v| v.constraint {} }
+    group = nil
+    group2 = DeltaRed.group do
+      group = DeltaRed.group do
+        constraints = vars.map { |v| v.constraint {} }
+      end
     end
     assert constraints.all? { |c| !c.enabled? }
     group.enable
     assert constraints.all? { |c| c.enabled? }
     group.disable
+    assert constraints.all? { |c| !c.enabled? }
+    group2.enable
+    assert constraints.all? { |c| c.enabled? }
+    group2.disable
     assert constraints.all? { |c| !c.enabled? }
   end
 end
